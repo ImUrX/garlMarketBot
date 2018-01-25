23fe8ee3c63b0e293a3cd1829667683dd2cdee51
Uriel <urielfontan2002@gmail.com>
2018-01-25 16:27:14 -0300

    added commands

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const config = require("./config.json");
 const Discord = require("discord.js");
 const snoowrap = require("snoowrap");
+const djsversion = require("./node_modules/discord.js/package.json");
+const botversion = require("./package.json");
 let lastPost = null;
 
 const client = new Discord.Client();
@@ -24,7 +26,7 @@ async function reddit() {
         .setTitle(json[0].title)
         .setURL(json[0].url)
         .setDescription(json[0].selftext);
-    client.channels.get(config.channel).send("**New post at /r/GarlicMarket!**", { embed });
+    client.channels.get(config.channelNotification).send("**New post at /r/GarlicMarket!**", { embed });
     console.log(`Sent a new message about ${json[0].title} with ID ${json[0].name}`);
 }
 
@@ -33,4 +35,39 @@ client.on("ready", () => {
     client.setInterval(reddit, 20 * 1000);
 });
 
+const cmds = {
+    "botstats": function(data) {
+        const embed = new Discord.RichEmbed()
+            .setColor("PURPLE")
+            .addField("Uptime:", uptime(process.uptime()), true)
+            .addField("Current RAM usage:", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB`, true)
+            .addField("Node.js Version:", process.version, true)
+            .addField("Bot's Version:", `v${botversion.version}`, true)
+            .addField("Discord.js Version:", `v${djsversion.version}`, true);
+        return data.channel.send("**Statistics**", { embed });
+    },
+    "restart": async function (data) {
+        if(!data.guild || !data.member.permissions.has("MANAGE_GUILD")) return;
+        await data.channel.send("Restarting...");
+        process.exit();
+    },
+    "help": function (data) {
+        data.channel.send("**Commands**: " + Object.keys(cmds).join(", "));
+    }
+};
+
+client.on("message", data => {
+    if(!data.content.startsWith(config.prefix)) return;
+    const command = data.content.substr(1).split(" ");
+    if (!(command[0] in cmds) || !(config.botChannel.includes(data.channel.guild.id))) return;
+    cmds[command[0]](data, command[1]);
+});
+
 client.login(config.token);
+
+function uptime(seconds) {
+    const numdays = Math.floor((seconds % 31536000) / 86400);
+    const numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
+    const numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+    return (numdays + "d " + numhours + "h " + numminutes + "m");
+}
